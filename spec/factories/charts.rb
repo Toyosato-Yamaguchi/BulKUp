e6b17b29199af3974a523adca66085cf5af3793b
Toyosato-Yamaguchi <gmail877toyosato@gmail.com>
2021-04-01 17:53:52 +0900

    摂取栄養素管理機能モデル単体テストコード

--- spec/factories/charts.rb
+++ spec/factories/charts.rb
@@ -2,15 +2,15 @@ FactoryBot.define do
   factory :chart do
     chart_date    {Faker::Date.between(from: '2020-01-01', to: '2022-01-01')}
     weight        {Faker::Number.number(digits: 3)}
-    calorie       {Faker::Number.between(from: 1, to: 4000)}
-    protein       {Faker::Number.between(from: 1, to: 100)}
-    lipid         {Faker::Number.between(from: 1, to: 100)}
-    carbohydorate {Faker::Number.betwenn(from: 1, to: 100)}
-    vitamin_B1    {Faker::Number.between(form: 0.0, to: 5.0)}
-    vitamin_B2    {Faker::Number.between(form: 0.0, to: 5.0)}
-    vitamin_B6    {Faker::Number.between(form: 0.0, to: 5.0)}
-    vitamin_C     {Faker::Number.between(form: 0.0, to: 5.0)}
-    vitamin_D     {Faker::Number.between(form: 0.0, to: 5.0)}
+    calorie       {Faker::Number.between(from: 0.0, to: 4000.0)}
+    protein       {Faker::Number.between(from: 0.0, to: 100.0)}
+    lipid         {Faker::Number.between(from: 0.0, to: 100.0)}
+    carbohydorate {Faker::Number.between(from: 0.0, to: 100.0)}
+    vitamin_B1    {Faker::Number.between(from: 0.00, to: 5.00)}
+    vitamin_B2    {Faker::Number.between(from: 0.00, to: 5.00)}
+    vitamin_B6    {Faker::Number.between(from: 0.00, to: 5.00)}
+    vitamin_C     {Faker::Number.between(from: 0.00, to: 5.00)}
+    vitamin_D     {Faker::Number.between(from: 0.00, to: 5.00)}
 
     association :user
   end
--- spec/models/chart_spec.rb
+++ spec/models/chart_spec.rb
@@ -8,54 +8,70 @@ RSpec.describe Chart, type: :model do
     
     context '摂取栄養素が登録できるとき' do
       it '日付、体重、カロリー、タンパク質、脂質、炭水化物、ビタミンB1、ビタミンB2、ビタミンB6、ビタミンD、ビタミンCが必須であること' do
-        
+        expect(@chart).to be_valid
       end
     end
 
     context '摂取栄養素が登録できないとき' do
       it '日付が空だと登録できない' do
+        @chart.chart_date = ''
+        @chart.valid?
+        expect(@chart.errors.full_messages).to include("Chart date can't be blank")
       end
-      it '体重が空だと登録できない' do
-      end
-      it '体重が半角数字以外だと登録できない' do
-      end
-      it 'カロリーが空だと登録できない' do
+      it '体重が半角英数字以外だと登録できない' do
+        @chart.weight = '１'
+        @chart.valid?
+        expect(@chart.errors.full_messages).to include("Weight is not a number")
       end
       it 'カロリーが半角数字以外だと登録できない' do
-      end
-      it 'タンパク質が空だと登録できない' do
+        @chart.calorie = '１'
+        @chart.valid?
+        expect(@chart.errors.full_messages).to include("Calorie is not a number")
       end
       it 'タンパク質が半角数字以外だと登録できない' do
-      end
-      it '炭水化物が空だと登録できない' do
+        @chart.protein = '１'
+        @chart.valid?
+        expect(@chart.errors.full_messages).to include("Protein is not a number")
       end
       it '炭水化物が半角数字以外だと登録できない' do
-      end
-      it '脂質が空だと登録できない' do
+        @chart.carbohydorate = '１'
+        @chart.valid?
+        expect(@chart.errors.full_messages).to include("Carbohydorate is not a number")
       end
       it '脂質が半角数字以外だと登録できない' do
-      end
-      it 'ビタミンB1が空だと登録できない' do
+        @chart.lipid = '１'
+        @chart.valid?
+        expect(@chart.errors.full_messages).to include("Lipid is not a number")
       end
       it 'ビタミンB1が半角数字以外だと登録できない' do
-      end
-      it 'ビタミンB2が空だと登録できない' do
+        @chart.vitamin_B1 = '１'
+        @chart.valid?
+        expect(@chart.errors.full_messages).to include("Vitamin b1 is not a number")
       end
       it 'ビタミンB2が半角数字だと登録できない' do
-      end
-      it 'ビタミンB6が空だと登録できない' do
+        @chart.vitamin_B2 = '１'
+        @chart.valid?
+        expect(@chart.errors.full_messages).to include("Vitamin b2 is not a number")
       end
       it 'ビタミンB6が半角数字だと登録できない' do
-      end
-      it 'ビタミンDが空だと登録できない' do
+        @chart.vitamin_B6 = '１'
+        @chart.valid?
+        expect(@chart.errors.full_messages).to include("Vitamin b6 is not a number")
       end
       it 'ビタミンDが半角数字以外だと登録できない' do
-      end
-      it 'ビタミンCが空だと登録できない' do
+        @chart.vitamin_D = '１'
+        @chart.valid?
+        expect(@chart.errors.full_messages).to include("Vitamin d is not a number")
       end
       it 'ビタミンCが半角数字だと登録できない' do
+        @chart.vitamin_C = '１'
+        @chart.valid?
+        expect(@chart.errors.full_messages).to include("Vitamin c is not a number")
       end
       it 'ユーザーが紐づいていないと登録できない' do
+        @chart.user = nil
+        @chart.valid?
+        expect(@chart.errors.full_messages).to include("User must exist")
       end
 
     end
