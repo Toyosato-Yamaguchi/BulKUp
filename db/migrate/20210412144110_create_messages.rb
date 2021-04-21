c10f97008dd897f649098f8058888822512361ef
BulkUp <gucci877toyosato@gmail.com>
2021-04-21 23:44:58 +0900

    AWSの設定

--- db/migrate/20210412144110_create_messages.rb
+++ db/migrate/20210412144110_create_messages.rb
@@ -2,8 +2,8 @@ class CreateMessages < ActiveRecord::Migration[6.0]
   def change
     create_table :messages do |t|
       t.text :text
-      t.integer :user_id
-      t.integer :recipe_id
+      t.integer :user_id, null: false
+      t.integer :recipe_id, null: false
       t.timestamps
     end
   end
