b7e5d49af2f1c8bbc66ecbe511b097708397da96
BulkUp <gucci877toyosato@gmail.com>
2021-04-23 23:39:38 +0900

    お気に入り機能ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,11 @@ Rails.application.routes.draw do
       get 'search'
     end
     resources :messages, only: :create
+    resources :favorites, only: [:create, :destroy]
   end
 
   resources :charts
-  resources :users, only: :show
+  resources :users, only: :show do
+    get :favorites, on: :collection
+  end
 end
